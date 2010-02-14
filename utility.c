f58ad04522e3d4491534007d97b687a00eb8fce5
Chris Ross <ctr@cention.se>
2010-02-14 18:23:02 +0000

    More files, needs cleaning up and not promised to work

---
+++ utility.c
@@ -0,0 +1,30 @@
+
+#include <libmemcached/memcached.h>
+#include "utility.h"
+
+static memcached_return_t memcached_key_process(
+	memcached_st *ptr, 
+	const char *key, 
+	size_t key_length, 
+	void *_context ) {
+		FeriteMemcached *context = (FeriteMemcached*)_context;
+		FeriteScript *script = context->script;
+		FeriteObject *block = context->block;
+		FeriteVariable *key_variable = ferite_create_string_variable_from_ptr( script, "", key, key_length, FE_CHARSET_DEFAULT, FE_STATIC );
+		FeriteVariable *params[2];
+		FeriteFunction *function = NULL;
+		
+		params[0] = key_variable;
+		params[1] = NULL;
+		
+		function = ferite_object_get_function_for_params( script, block, "invoke", params );
+		if( function ) {
+			FeriteVariable *return_variable = ferite_call_function( script, block, NULL, function, params );
+			if( return_variable ) {
+				ferite_variable_destroy( script, return_variable );
+			}
+		}
+		
+		ferite_variable_destroy( script, key_variable );
+		return MEMCACHED_SUCCESS;
+}
---
+++ utility.h
@@ -0,0 +1,9 @@
+
+#include <ferite.h>
+
+typedef struct __ferite_memcached {
+	FeriteScript *script;
+	FeriteObject *block;
+} FeriteMemcached;
+
+static memcached_return_t memcached_key_process( memcached_st *ptr, const char *key, size_t key_length, void *context );
